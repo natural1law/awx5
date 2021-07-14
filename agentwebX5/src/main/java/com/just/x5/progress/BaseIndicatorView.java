18c6fb54bf4c8dc83639b311cd048c0fad495308
natural1law <17640051884@163.com>
2021-07-14 13:20:43 +0800

    Initial commit

---
+++ agentwebX5/src/main/java/com/just/x5/progress/BaseIndicatorView.java
@@ -0,0 +1,53 @@
+package com.just.x5.progress;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+
+import androidx.annotation.Nullable;
+
+import com.just.x5.ILayoutParamsOffer;
+
+/**
+ * 进度条基类
+ */
+
+public abstract class BaseIndicatorView extends FrameLayout implements IBaseProgressSpec, ILayoutParamsOffer {
+    public BaseIndicatorView(Context context) {
+        super(context);
+    }
+
+    public BaseIndicatorView(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public BaseIndicatorView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @Override
+    public void reset() {
+
+    }
+
+    @Override
+    public void setProgress(int newProgress) {
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+
+    @Override
+    public LayoutParams offerLayoutParams() {
+        return null;
+    }
+}
